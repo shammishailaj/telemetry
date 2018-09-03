aef2bc1a84a8234e5738d8ef8917c587a5aa09cb
rijkvanzanten <rijkvanzanten@me.com>
2018-09-03 12:04:45 -0400

    Fetch records from DB on GET /versions

--- src/app.js
+++ src/app.js
@@ -4,4 +4,18 @@ const db = require("./db");
 
 const app = express();
 
+app.get("/versions", (req, res) => {
+  db
+    .select("*")
+    .from("versions")
+    .orderBy("date")
+    .then(records => {
+      res.send(records);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).end();
+    });
+});
+
 module.exports = app;
--- src/db.js
+++ src/db.js
@@ -2,10 +2,12 @@ const { host, user, password, database } = process.env;
 
 const db = require("knex")({
   client: "mysql",
-  host,
-  user,
-  password,
-  database
+  connection: {
+    host,
+    user,
+    password,
+    database
+  }
 });
 
 module.exports = db;
